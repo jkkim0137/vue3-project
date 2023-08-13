beee1db12dcda076e8da7173c0e6edb99c647386
jkkim <jkkim0137@gmail.com>
2023-08-13 13:42:52 +0900

    설정 수정

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import './assets/main.css'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
