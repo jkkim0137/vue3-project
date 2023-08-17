00ed5345a27074ac6d06a336fe30a9ebdbd48cff
jkkim <jkkim0137@gmail.com>
2023-08-17 16:31:19 +0900

    차트 데이터 시각화 작업중

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -2,28 +2,50 @@
   <div class="dashboard-wrap bg-gray-100">
     <div class="flex flex-col gap-4">
       <div class="flex flex-row gap-4">
-        <el-card class="box-card h-80 basis-1/3"> <DoughnutChart :chartData="testData" /></el-card>
-        <el-card class="box-card h-80 basis-1/3"> </el-card>
-        <el-card class="box-card h-80 basis-1/2"> </el-card>
+        <el-card class="flex justify-center box-card h-90 basis-1/3"
+          ><DoughnutChart :chartData="testData"
+        /></el-card>
+        <el-card class="flex justify-center box-card h-90 basis-1/3"
+          ><LineChart :chartData="testData"
+        /></el-card>
+        <el-card class="flex justify-center box-card h-90 basis-1/2"
+          ><BubbleChart :chartData="testData"
+        /></el-card>
       </div>
-      <div class="flex flex-row gap-4">
-        <el-card class="box-card h-80 basis-1/3"> </el-card>
-        <el-card class="box-card h-80 basis-1/3"> </el-card>
-        <el-card class="box-card h-80 basis-1/2"> </el-card>
-      </div>
+      <!-- <div class="flex flex-row gap-4">
+        <el-card class="flex justify-center box-card h-90 basis-1/3"
+          ><DoughnutChart :chartData="testData"
+        /></el-card>
+        <el-card class="flex justify-center box-card h-90 basis-1/3"
+          ><LineChart :chartData="testData"
+        /></el-card>
+        <el-card class="flex justify-center box-card h-90 basis-1/2"
+          ><BubbleChart :chartData="testData"
+        /></el-card>
+      </div> -->
     </div>
   </div>
 </template>
 <script setup>
-import { DoughnutChart } from 'vue-chart-3'
+import { ref, watchEffect } from 'vue'
+import { DoughnutChart, LineChart, BubbleChart } from 'vue-chart-3'
 import { Chart, registerables } from 'chart.js'
 Chart.register(...registerables)
 
+const donutChartData = ref([
+  { label: '음식', value: 3000 },
+  { label: '패션', value: 1500 },
+  { label: '전자제품', value: 1200 },
+  { label: '서적', value: 800 },
+  { label: '운동', value: 600 },
+  { label: '기타', value: 300 }
+])
+
 const testData = {
-  labels: ['Paris', 'Nîmes', 'Toulon', 'Perpignan', 'Autre'],
+  labels: donutChartData.value.map((item) => item.label),
   datasets: [
     {
-      data: [30, 40, 60, 70, 5],
+      data: donutChartData.value.map((item) => item.value),
       backgroundColor: ['#77CEFF', '#0079AF', '#123E6B', '#97B0C4', '#A5C8ED']
     }
   ]
